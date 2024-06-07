67bf55bd691cd13fef3ad073c8a4f1c22d242ab0
nwrenger <nils@wrenger.net>
2024-06-08 00:05:54 +0200

    :wrench: Fixed styling

--- src/main.rs
+++ src/main.rs
@@ -97,14 +97,18 @@ fn navbar() -> Markup {
 /// The Page, useful when wanting to use multiple Pages
 fn page(title: &str, html: Markup) -> Markup {
     html! {
-        div class="grid grid-rows-[64px_auto] h-full" {
-            div {
-                (header(title))
-                (navbar())
-            }
-            div class="h-full overflow-scroll" {
-                div class="container space-y-8 flex flex-col items-center !max-w-6xl mx-auto p-4" {
-                    (html)
+        head {
+            (header(title))
+        }
+        body {
+            div class="grid grid-rows-[64px_auto] h-full" {
+                div {
+                    (navbar())
+                }
+                div class="h-full overflow-scroll" {
+                    div class="container space-y-8 flex flex-col items-center !max-w-6xl mx-auto p-4" {
+                        (html)
+                    }
                 }
             }
         }
